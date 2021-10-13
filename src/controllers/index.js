3607a3d3a6d96d80ef18f31d3f8b64ad98ab1a41
Phạm Cao Toàn <70565574+toanphamcao@users.noreply.github.com>
2021-10-13 18:38:07 +0700

    Add files via upload

---
+++ src/controllers/index.js
@@ -0,0 +1,6 @@
+module.exports.authController = require('./auth.controller');
+module.exports.userController = require('./user.controller');
+module.exports.lopController = require('./lop.controller');
+module.exports.sinhvienController = require('./sinhvien.controller');
+
+
---
+++ src/models/index.js
@@ -0,0 +1,6 @@
+module.exports.Token = require('./token.model');
+module.exports.User = require('./user.model');
+module.exports.Lop = require('./lop.model');
+module.exports.Sinhvien = require('./sinhvien.model');
+
+
